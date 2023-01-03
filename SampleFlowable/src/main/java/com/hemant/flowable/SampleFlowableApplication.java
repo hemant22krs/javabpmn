5a37f736d45b6c1021cbf86ae15bdeeff225dee0
hemant22krs <January@2023>
2023-01-03 16:20:32 +0530

    Project Added

---
+++ SampleFlowable/src/main/java/com/hemant/flowable/SampleFlowableApplication.java
@@ -0,0 +1,13 @@
+package com.hemant.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleFlowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleFlowableApplication.class, args);
+    }
+
+}
